ea69b0112040dffbb1713eb7b6ba93177173cda8
Santosh0208 <santoshalamuru0@msitprogram.net>
2022-02-02 11:14:50 +0530

    Generate Text Report

--- hw6_protein.py
+++ hw6_protein.py
@@ -200,6 +200,25 @@ Parameters: 2D list of strs ; 2D list of values
 Returns: None
 '''
 def displayTextResults(commonalities, differences):
+    list = []
+    list1 = []
+    str = ''
+    for j in commonalities:
+        if j not in list:
+            list.append(j[1:-1])
+    for k in list:
+        if len(k) > 0:
+            k = '-'.join(k)
+            list1.append(k)
+    list1.sort()
+    for l in list1:
+        str += ' '+ l +"\n"
+    print(str)
+    for b in differences:
+        wrd = b[0]
+        seq1 = round(b[1]*100,2)
+        seq2 = round(b[2]*100,2)
+        print(f"{wrd}:{seq1}% in Seq1, {seq2}% in seq2")
     return
 
 
